999e27f7c8035ae2426cd277c5a9a785e5d64f19
Alvin Alic <alvin.alic@gmail.com>
2024-05-21 18:17:15 -0500

    Create function that willtake user prompt and create new grid

--- javascript.js
+++ javascript.js
@@ -13,23 +13,61 @@ function createGrid(){
             }
         }
         
-        
-        // const column = document.createElement("div");
-        // column.className = 'column';
-        // if(i % 16 == 0){
-        //     const column = document.createElement("div");
-        //     column.className = 'column';
-        //     grid.append(column);
-        // }
-        // const square = document.createElement("div")
-        // square.className = 'square'; 
-        // grid.append(square);
     }
     
 }
 
 
-createGrid();
+// createGrid();
+
+// let sqr = document.querySelectorAll(".square");
+
+
+
+// sqr.forEach(sqr =>{
+//     sqr.addEventListener("mouseover", () =>{
+//         sqr.style.cssText = "color: blue; background: blue";
+//     })
+// });
+
+let button = document.querySelector(".button");
+
+button.addEventListener("click", () =>{
+    let answer = prompt("How many squares would you like?");
+    newGrid(answer);
+    let sqr = document.querySelectorAll(".square");
+
+
+
+sqr.forEach(sqr =>{
+    sqr.addEventListener("mouseover", () =>{
+        sqr.style.cssText = "color: blue; background: blue";
+    })
+});
+
+})
+
+function newGrid(numberOfSquares){
+    if(numberOfSquares > 100){
+        alert("Must be less than 101");
+        return;
+    }
+    let grid = document.querySelector(".grid");
+    for(let i = 0; i < numberOfSquares*numberOfSquares; i++)
+        {
+            if(i%16 == 0){
+                const column = document.createElement("div");
+                column.className = 'column';
+                grid.append(column);
+                for(let j = 0; j < 16; j++){
+                    const square = document.createElement("div");
+                    square.className = 'square';
+                    column.append(square);
+                }
+            }
+        }
+}
+
 
 let sqr = document.querySelectorAll(".square");
 
